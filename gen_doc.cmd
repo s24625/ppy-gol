8cad53b9a4708c5f72c0a4172a0656f88f3ade08
s24625 <s24625+2@pjwstk.edu.pl>
2025-06-10 10:30:31 +0200

    Update gen_doc.cmd

--- gen_doc.cmd
+++ gen_doc.cmd
@@ -3,4 +3,4 @@ pip install pdoc3
 REM Generacja HTML
 bin\pdoc3 --html --output-dir docs game_of_life.py --force
 REM Konwersja HTML na PDF
-bin\wkhtmltopdf docs/game_of_life.html docs/game_of_life.pdf --load-error-handling ignore
+bin\wkhtmltopdf docs\game_of_life.html docs\game_of_life.pdf
